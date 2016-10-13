d2faf2c7c651774c1398667d0d4607d5bdf52471
Danilo Alfredo Marinho de Souza <dams@cin.ufpe.br>
2016-10-13 13:46:50 -0300

    Perceptron com graficos e começo do Adaline

--- Perceptron.py
+++ Perceptron.py
@@ -1,5 +1,7 @@
 import numpy as np
 import random as rnd
+import matplotlib.pyplot as plt
+import os, subprocess
 
 class Perceptron:
 	def __init__(self, N):
@@ -11,12 +13,13 @@ class Perceptron:
 	def classify(W, data):
 		s = int(np.sign(W.T.dot(data)))
 		return s
+		
 	def point_gen(self, N):
 		X = []
 		for i in range(N):
 			x1, x2 = [rnd.uniform(-1, 1) for i in range(2)] #gera pontos aleatórios
 			x = np.array([1, x1, x2])
-			s = self.classify(self.V, x)
+			s = int(np.sign(self.V.T.dot(x)))
 			X.append((x, s))
 		return X
 
@@ -57,23 +60,42 @@ class Perceptron:
 
 	def trainingVanilla(self, it, save=False):
 		#inicializa pesos aleatórios
-		w = rnd.rand(3)
+		w = np.zeros(3)
 		for i in xrange(it):
 			msPt = []
 			for Xi in self.X:
 				vec = Xi[0]
 				classification = Xi[1]
-				out = self.classify(w, vec)
+				out = int(np.sign(w.T.dot(vec)))
 				if out != classification:
 					w += classification*vec
 					msPt.append((vec, classification))
 			if save:
 				self.plot(vec=w)
-				plt.title('N = %s, Iteration %s' % (str(len(self.X)), str(it)))
-				plt.savefig('p_N%s_it%s' % (str(len(self.X), str(it))), dpi=200, bbox_inches='tight') 
+				plt.title('N = %s, Iteration %s' % (str(len(self.X)), str(i+1)))
+				plt.savefig('p_N%s_it%s' % (str(len(self.X)), str(i+1)), dpi=200, bbox_inches='tight') 
 		self.w = w
-	#def trainingAdaline(self, iter, eta):
+	def trainingAdaline(self, it, eta):
+		w = np.array([rnd.uniform(-1, 1) for i in range(3)])
+		for i in range(it):
+			msPt = []
+			for Xi in self.X:
+				vec = Xi[0]
+				classification = Xi[1]
+				out = int(np.sign(w.T.dot(vec)))
+				error = out - classification
+				w += error*eta*vec
 
 
-MCP = Perceptron(30)
-MCP.trainingVanilla(10, save=True)
+
+MCP = Perceptron(80)
+MCP.trainingVanilla(20, save=True)
+basedir = 'C:\Users\danilo.souza\Documents\UFPE\Neural Network Playground'
+os.chdir(basedir)
+pngs = [pl for pl in os.listdir(basedir) if pl.endswith('png')]
+sortpngs = sorted(pngs, key=lambda a:int(a.split('it')[1][:-4]))
+basepng = pngs[0][:-8]
+[sortpngs.append(sortpngs[-1]) for i in range(4)]
+comm = ("convert -delay 50 %s %s.gif" % (' '.join(sortpngs), basepng)).split()
+proc = subprocess.Popen(comm, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+(out, err) = proc.communicate()
